c2295d12fc623f5d54fc462b14eb330438956779
Tuguldur Gantumur <gtrtuugii@gmail.com>
2023-09-06 21:54:35 +1000

    update chrome_path to environment variable

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -6,31 +6,10 @@ html_file="timesheet.html"
 # Define the PDF output file path
 pdf_output="output.pdf"
 
-# Windows
-# path_to_chrome="C:\Program Files\Google\Chrome\Application\chrome.exe"
-
-# Determine the user's operating system
-os_name=$(uname -s)
-
-# Set the path to Chrome based on the operating system
-case $os_name in
-  Linux)
-    # Linux path
-    path_to_chrome="/usr/bin/google-chrome"
-    ;;
-  Darwin)
-    # macOS path
-    path_to_chrome=/Applications/"Google\ Chrome.app"/Contents/MacOS/"Google\ Chrome"
-    ;;
-  *)
-    # Default path (modify as needed)
-    echo "Unsupported operating system: $os_name"
-    exit 1
-    ;;
-esac
+chrome_bin=${CHROME_BIN:-"chromium-browser"}
 
 # Run Chrome Headless in headless mode to generate the PDF
-    eval $path_to_chrome  \
+    eval $chrome_bin  \
     --headless  \
     --disable-gpu \
     --print-to-pdf=$pdf_output \
