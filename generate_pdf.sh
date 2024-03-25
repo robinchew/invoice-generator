fa733b780979f09e95ac47f490a51c29ec7ac14d
ungodlyaura <120505880+ungodlyaura@users.noreply.github.com>
2024-03-25 11:34:46 +0800

    Tax Added

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
+set -e
+
+if [ -z $1 ];then
+    echo "1 argument required";
+    exit 1;
+fi;
 
 # Define the HTML input file path
-html_file="timesheet.html"
+html_file=$1
 
-# Define the PDF output file path
-pdf_output="output.pdf"
-
-chrome_bin=${CHROME_BIN:-"chromium-browser"}
+chrome_bin=${CHROME_BIN:-"chromium"}
 
 # Run Chrome Headless in headless mode to generate the PDF
+for qs in invoice timesheet;do
+    pdf_output="${html_file%.*}_$qs.pdf";
+    input=file://`readlink -f $html_file`?$qs;
+    echo $input;
     eval $chrome_bin  \
     --headless  \
     --disable-gpu \
     --print-to-pdf=$pdf_output \
-    $html_file
+    $input;
 
-
-# Check if PDF generation was successful
-if [ -f "$pdf_output" ]; then
-    echo "PDF has been generated."
-else
-    echo "PDF generation failed."
-fi
+    # Check if PDF generation was successful
+    if [ -f "$pdf_output" ]; then
+        echo "PDF has been generated."
+    else
+        echo "PDF generation failed."
+    fi
+done
