eeafdcf1af9ab37dfbbac9cfd302373895a753e9
ungodlyaura <120505880+ungodlyaura@users.noreply.github.com>
2024-05-23 12:40:19 +0800

    should open html in chrome

--- generate_pdf.sh
+++ generate_pdf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+which chromium
 
 if [ -z $1 ];then
     echo "1 argument required";
@@ -9,23 +10,14 @@ fi;
 # Define the HTML input file path
 html_file=$1
 
-chrome_bin=${CHROME_BIN:-"chromium"}
+chrome_bin=${CHROME_BIN:-"google-chrome"}
 
 # Run Chrome Headless in headless mode to generate the PDF
 for qs in invoice timesheet;do
-    pdf_output="${html_file%.*}_$qs.pdf";
     input=file://`readlink -f $html_file`?$qs;
     echo $input;
     eval $chrome_bin  \
     --headless  \
     --disable-gpu \
-    --print-to-pdf=$pdf_output \
     $input;
-
-    # Check if PDF generation was successful
-    if [ -f "$pdf_output" ]; then
-        echo "PDF has been generated."
-    else
-        echo "PDF generation failed."
-    fi
 done
